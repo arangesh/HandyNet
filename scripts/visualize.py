3269dfd68816939b368cccb35df11acb1fffd47a
Akshay Rangesh <arangesh@ucsd.edu>
2020-01-07 12:08:06 -0800

    Update visualize.py
    Minor fix.

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -147,7 +147,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    plt.show(block=False)
+    plt.show(block=True)
     time.sleep(display_time)
     plt.close()
     
